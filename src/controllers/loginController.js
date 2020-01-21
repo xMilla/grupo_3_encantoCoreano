0457202e4c231634b8d84cf7e6ac9aaa0ad384cc
flor <mfcaldao@gmail.com>
2020-01-20 22:47:02 -0300

    ersion final

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -5,12 +5,9 @@ const loginController = {
 	login: (req, res) => {
 		res.render('login');
 	},
-<<<<<<< HEAD
-=======
 	registro: (req, res) => {
 		res.render('registro');
 	},
->>>>>>> a29b505f61aed26ceed07a80eb39f71c26d4962b
 };
 
 module.exports = loginController
