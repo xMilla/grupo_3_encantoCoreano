485b955732b15aeae9175255b11f03caa067a975
flor <mfcaldao@gmail.com>
2020-01-20 22:33:16 -0300

    fix en master

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,12 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const helperFunctions = require('../functions/helpers');
 
-<<<<<<< HEAD
-
-const controller = {
-	root: (req, res) => {
-		res.render('index');
-=======
 let productsData = helperFunctions.getAll('todos');
 
 const controller = {
@@ -23,7 +17,6 @@ const controller = {
 
 	login:(req, res) => {
 		res.render('login');
->>>>>>> a29b505f61aed26ceed07a80eb39f71c26d4962b
 	},
 	carrito: (req, res) => {
 		res.render('Carrito');
